41225c3f63facf9e3f2c174fe7c83d083a2ab98a
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-07-28 19:34:51 +0300

    Add: network

--- ClientServerCalculatur_WPF/MainWindow.xaml.cs
+++ ClientServerCalculatur_WPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ClientServerCalculatur_WPF
 {
@@ -21,10 +12,24 @@ namespace ClientServerCalculatur_WPF
     public partial class MainWindow : Window
     {
         private static MainWindow? s_window;
+        private const int PORT = 8081;
+        private const string IP = "127.0.0.1";
+        IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP), PORT);
+        private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
         public MainWindow()
         {
             InitializeComponent();
             s_window = this;
+            clientSocket.Connect(iPEnd);
+            //this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (clientSocket.Connected)
+            //clientSocket.Connect(iPEnd);
         }
 
         #region --- Header ---
@@ -50,7 +55,7 @@ namespace ClientServerCalculatur_WPF
         #region --- Opertions ---
         private void SumButton_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void SubtractButton_Click(object sender, RoutedEventArgs e)
@@ -70,8 +75,37 @@ namespace ClientServerCalculatur_WPF
 
         private void ResultButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ConnectionToServer();
         }
+        #endregion
+
+        #region --- Methods ---
+        private void ConnectionToServer()
+        {
+        
+            string message = DisplayOutput.Text;
+            var data = Encoding.UTF8.GetBytes(message);
+
+            //clientSocket.Connect(iPEnd);
+            clientSocket.Send(data);
+
+            byte[] buffer = new byte[256];
+            int dataSize = 0;
+            StringBuilder answerString = new StringBuilder();
+
+            do
+            {
+                dataSize = clientSocket.Receive(buffer);
+                answerString.Append(Encoding.UTF8.GetString(buffer, 0, dataSize));
+
+            }
+            while (clientSocket.Available > 0);
+
+            ServerTerminalText.Text = $"{answerString.ToString()}\n";
+            //clientSocket.Shutdown(SocketShutdown.Both);
+            //clientSocket.Close();
+            
+}
         #endregion
     }
 }
---
+++ Server/Calculator.cs
@@ -0,0 +1,107 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Calculator
+    {
+        private int _firstnumber;
+        private int _secondNumber;
+        private int _calculateResult;
+        private char _mathOperation;
+
+        private const int PORT = 8081;
+        private const string IP = "127.0.0.1";
+        private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public char MathOperation
+        {
+            get { return _mathOperation; }
+            set { _mathOperation = value; }
+        }
+
+        public int CalculateResult
+        {
+            get { return _calculateResult; }
+            set { _calculateResult = value; }
+        }
+
+        public int SecondNumber
+        {
+            get { return _secondNumber; }
+            set { _secondNumber = value; }
+        }
+
+        public int FirstNumber
+        {
+            get { return _firstnumber; }
+            set { _firstnumber = value; }
+        }
+
+        private int Sum(int firstNum, int secondNum) => _calculateResult = firstNum + secondNum;
+        
+        private int Substract(int firstNum, int secondNum) => _calculateResult = firstNum - secondNum;
+        
+        private int Multiply(int firstNum, int secondNum) => _calculateResult = firstNum * secondNum;
+        
+        private int Devide(int firstNum, int secondNum) => _calculateResult = firstNum / secondNum;
+        
+        private bool NullDevideResult(int firstNum, int secondNum)
+        {
+            if ((firstNum == 0) || (secondNum == 0)) { return false; }
+            else return true;
+        }
+
+        public void ServerUp()
+        {
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse(IP), PORT);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(iPEnd);
+            serverSocket.Listen(5);
+            Console.WriteLine("Сервер поднят");
+
+            while (true)
+            {
+                var listner = serverSocket.Accept();
+                byte[] buffer = new byte[256];
+                int dataSize = 0;
+                StringBuilder dataString = new StringBuilder();
+
+                do
+                {
+                    dataSize = listner.Receive(buffer);
+                    dataString.Append(Encoding.UTF8.GetString(buffer, 0, dataSize));
+                }
+                while (listner.Available > 0);
+
+                Console.WriteLine(dataString.ToString());
+                listner.Send(Encoding.UTF8.GetBytes("Ok!"));
+                //listner.Shutdown(SocketShutdown.Both);
+                //listner.Close();
+            }
+        }
+
+        public void ListenClient()
+        {
+            var listner = serverSocket.Accept();
+            byte[] buffer = new byte[256];
+            int dataSize = 0;
+            StringBuilder dataString = new StringBuilder();
+
+            do
+            {
+                dataSize = listner.Receive(buffer);
+                dataString.Append(Encoding.UTF8.GetString(buffer, 0, dataSize));
+
+            }
+            while(listner.Available > 0);
+
+            Console.WriteLine(dataString.ToString());
+
+            listner.Send(Encoding.UTF8.GetBytes("Ok!"));
+            listner.Shutdown(SocketShutdown.Both);
+            listner.Close();
+        }
+    }
+}
